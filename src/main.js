f38bc4d47b9e37a1e37fefe25dfbe39409b5dd37
Furrazlhf <brian.lecarpentier@edu.itescia.fr>
2020-09-29 11:34:05 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import AsyncComputed from 'vue-async-computed';
-import { store } from './store/index'
+import { store } from '@/store'
 
 Vue.use(AsyncComputed)
 
