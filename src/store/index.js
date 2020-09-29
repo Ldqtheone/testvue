0cd7ab099b75e58ab5c78a3d2adcc5fe09c97b81
Furrazlhf <brian.lecarpentier@edu.itescia.fr>
2020-09-29 16:24:15 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,8 @@ Vue.use(Vuex);
 
 export const store = new Vuex.Store({
     state: {
-        errorMessage: ""
+        errorMessage: "",
+        city: "Rennes"
     },
     mutations: {
         displayError(state, errorMessage) {
